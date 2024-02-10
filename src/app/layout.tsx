8d21b868384dbae25f4561c561e9c40c934751c7
Alex Caballero <alex.caballero.4@gmail.com>
2024-02-10 16:12:54 +0100

    Create Next.js project from template

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Strava Next.js",
+  description: "Next.js app to create & visualize workouts with Strava API.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
